4d6fc63ee2175e3d5e50425af65ed62069d774db
palloc <palloc.mknet10@gmail.com>
2016-06-26 01:00:31 +0900

    フィルタ実装

--- main.c
+++ main.c
@@ -115,6 +115,64 @@ int main(){
 
 	/*
 	  ----------------------------
+	     ここからフィルタリング
+	  ----------------------------
+	*/
+	printf("\n----------------------\n");
+	printf("    Start Filtering   \n");
+	printf("----------------------\n");	
+	/* 100*100より外を0にする理想的フィルタリング */
+	for( i=0; i<N; i++ ){
+		for( j=0; j<N; j++ ){
+			if( i<N/2-50 || i>N/2+50 || j<N/2-50 || j>N/2+50 ){
+				F_Real[i][j] = 0;
+				F_Im[i][j] = 0;
+			}
+		}
+	}
+	
+	/* 
+	-------------------------
+	  パワースペクトル生成 
+	-------------------------
+	*/
+	max[0] = 0;
+	for( i=0; i<N; i++ ){
+		for( j=0; j<N; j++ ){
+			Power_spectrum[i][j] = log10(pow(F_Real[i][j], 2)+pow(F_Im[i][j], 2));
+			//最大値を求めている(あとで正規化に使う)
+			if( max[0] < Power_spectrum[i][j] ){
+				max[0] = Power_spectrum[i][j];
+				max[1] = i;
+				max[2] = j;
+			}
+		}
+	}
+	/* データの正規化 */
+	ratio = 254 / Power_spectrum[(int)max[1]][(int)max[2]];
+	for( i=0; i<N; i++ ){
+		for( j=0; j<N; j++ ){
+			Power_spectrum[i][j] *= ratio;
+			read_file[i][j] = (unsigned char)Power_spectrum[i][j];
+			printf("%d,",read_file[i][j]);
+		}
+	}
+	
+	fp=fopen("image/After_Filter_lenna_Fourier.raw","wb+"); //フーリエ変換後のパワースペクトル
+	for( i=0; i<N; i++ ){
+		for( j=0; j<N; j++ ){
+			fwrite(&read_file[i][j], 1, 1, fp);
+		}
+	}
+	fclose(fp);
+
+	printf("\n----------------------\n");
+	printf("     Fin Filtering    \n");
+	printf("----------------------\n");	
+
+	
+	/*
+	  ----------------------------
 	      ここからIDFTの処理
 	  ----------------------------
 	*/
@@ -129,7 +187,7 @@ int main(){
 			for( k=0; k<N; k++ ){
 				for( l=0; l<N; l++ ){
 					//u=0,1,...,N-1 v=0,1,...,N-1
-					f_after[u][v] += F_Real[k][l] * cos(2.0 * M_PI * ((double)i*k + (double)j*l) / N) / (N*N);
+					f_after[u][v] += F_Real[k][l] * cos(2.0 * M_PI * ((double)i*k + (double)j*l) / N) / N + F_Im[k][l] * sin(2.0 * M_PI * ((double)i*k + (double)j*l) / N) / N;
 				}
 			}
 		}
